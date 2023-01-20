ab7bcd2f49a495af39c1c6dabb70aa63caf9b12e
tahasama <taha.maatof@gmail.com>
2023-01-20 17:40:42 +0100

    like and unlike functionality ok

--- Item.tsx
+++ Item.tsx
@@ -34,10 +34,13 @@ import {
   loadcomments,
   addcomments,
   removeComment,
+  addCommentLike,
+  isCommentLiked,
 } from "./state/reducers/commentsSlice";
 import { loadStories } from "./state/reducers/storiesSlice";
 import Entypo from "@expo/vector-icons/Entypo";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { ComplexAnimationBuilder } from "react-native-reanimated";
 
 const Item = ({ navigation, route }) => {
   const ccc = route.params;
@@ -48,9 +51,9 @@ const Item = ({ navigation, route }) => {
   const [comment, setComment] = useState("");
   const [data, setData] = useState([]);
   const [selectedId, setSelectedId] = useState(null);
-  console.log("ddddddddddd", user.id);
   const dispatch = useAppDispatch();
-  const { result } = useAppSelector(getcommentsData);
+  const { result, commentLiked } = useAppSelector(getcommentsData);
+  console.log("ddddddddddd", commentLiked);
 
   useEffect(() => {
     dispatch(storyRoute(ccc.item.id));
@@ -63,7 +66,13 @@ const Item = ({ navigation, route }) => {
   console.log("434343", user);
   const handleComment = async () => {
     dispatch(
-      addcomments({ comment: comment, userId: user.id, storyId: ccc.item.id })
+      addcomments({
+        comment: comment,
+        userId: user.id,
+        storyId: ccc.item.id,
+        likes: [],
+        dislikes: [],
+      })
     )
       .then(() => setStatus("success"))
       .then(() => setComment(""))
@@ -78,14 +87,75 @@ const Item = ({ navigation, route }) => {
     setSelectedId(item.id);
   };
 
+  const handleLike = (item) => {
+    console.log(item.likes.filter((zzz) => zzz.liker === user.id).length === 0);
+    const commentLikesData = { commentId: item.id, liker: user.id };
+    const commentLikesArray = [...item.likes];
+    item.likes.filter((zzz) => zzz.liker === user.id).length === 0
+      ? commentLikesArray.push(commentLikesData)
+      : commentLikesArray.pop();
+    dispatch(
+      addCommentLike({
+        commentLikesData,
+        commentLikesArray,
+      })
+    ).then(() => dispatch(isCommentLiked(!commentLiked)));
+    console.log("12313123123123", commentLiked);
+  };
+
+  const handleDislike = (item) => {
+    // dispatch(addCommentDislike(item.id))
+
+    setSelectedId(item.id);
+  };
+
   const getHeader = () => {
     return (
       <View style={styles.subContainer}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={{
+              uri: ccc.item.avatar,
+            }}
+            style={{
+              width: 30,
+              height: 30,
+              borderRadius: 50,
+              marginHorizontal: 10,
+              marginVertical: 18,
+            }}
+          />
+          <Text style={{ fontSize: 16, color: "white" }}>
+            {ccc.item.username}
+          </Text>
+        </View>
         <Text style={styles.title}>{ccc.item.title}</Text>
         <Text style={styles.content}>
           {"\t"}
 
           {ccc.item.content}
+        </Text>
+        <View
+          style={{
+            borderBottomColor: "grey",
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
+        />
+        <Text
+          style={{
+            color: "#7f6c33",
+            fontSize: 14,
+            marginLeft: 5,
+            marginTop: 3,
+          }}
+        >
+          Comments :
         </Text>
       </View>
     );
@@ -132,10 +202,7 @@ const Item = ({ navigation, route }) => {
                 /> */}
               <Image
                 source={{
-                  uri:
-                    item.avatar === ""
-                      ? "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
-                      : item.avatar,
+                  uri: item.avatar,
                 }}
                 style={{
                   width: 30,
@@ -156,23 +223,42 @@ const Item = ({ navigation, route }) => {
                   handleOnpress(item);
                 }}
               >
-                <Text style={[styles.comment, { color: "#727577", margin: 5 }]}>
+                <Text
+                  style={[
+                    styles.comment,
+                    { color: "#727577", margin: 5, fontSize: 15 },
+                  ]}
+                >
                   Reply
                 </Text>
               </TouchableOpacity>
               <TouchableOpacity
+                // disabled={commentLiked && true}
                 onPress={() => {
-                  handleOnpress(item);
+                  handleLike(item);
                 }}
               >
-                <Entypo name="arrow-bold-up" color={"#669393"} size={26} />
+                <View style={{ transform: [{ rotate: "40deg" }] }}>
+                  <Entypo
+                    name="arrow-bold-up"
+                    color={
+                      item.likes.filter((zzz) => zzz.liker === user.id)
+                        .length !== 0 || commentLiked
+                        ? "#9715a5"
+                        : "#4d759a"
+                    }
+                    size={26}
+                  />
+                </View>
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={() => {
-                  handleOnpress(item);
+                  handleDislike(item);
                 }}
               >
-                <Entypo name="arrow-bold-down" color={"#669393"} size={26} />
+                <View style={{ transform: [{ rotate: "40deg" }] }}>
+                  <Entypo name="arrow-bold-down" color={"#4d759a"} size={26} />
+                </View>
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={() => {
@@ -253,7 +339,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#051e28",
     // color: "yellow",
   },
-  subContainer: { marginBottom: 60 },
+  subContainer: { marginBottom: 10 },
   title: {
     fontSize: 32,
     color: "#CDD2D4",
@@ -265,6 +351,7 @@ const styles = StyleSheet.create({
     color: "#9BA5A9",
     paddingHorizontal: 20,
     paddingBottom: 20,
+    minHeight: 200,
   },
   commentResponseContainer: {
     borderTopWidth: 1,
@@ -283,7 +370,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   comment: {
-    fontSize: 20,
+    fontSize: 18,
     color: "#9BA5A9",
     paddingHorizontal: 20,
     paddingVertical: 10,
--- state/reducers/commentsSlice.ts
+++ state/reducers/commentsSlice.ts
@@ -7,6 +7,7 @@ import {
   getDoc,
   getDocs,
   query,
+  updateDoc,
   where,
 } from "firebase/firestore";
 import { Alert } from "react-native";
@@ -18,6 +19,8 @@ interface commentProps {
   userId: string;
   storyId: string;
   comment: string;
+  likes: any[];
+  dislikes: any[];
 }
 
 export const loadcomments = createAsyncThunk(
@@ -65,8 +68,33 @@ export const addcomments = createAsyncThunk(
         commenter: userId,
         storyId: storyId,
         timestamp: Date.now(),
+        likes: [],
+        dislikes: [],
       });
       console.log("the res is", res);
+      return res;
+    } catch (e) {
+      console.error("Error adding document: ", e);
+      Alert.alert("action failed please try again");
+    }
+  }
+);
+
+export const addCommentLike = createAsyncThunk(
+  "addCommentLike",
+  async (commentLikesInofs: any) => {
+    try {
+      console.log("frfrfrrfr", commentLikesInofs);
+
+      const res = await updateDoc(
+        doc(db, "comments", commentLikesInofs.commentLikesData.commentId),
+        {
+          likes: commentLikesInofs.commentLikesArray,
+        }
+      );
+
+      console.log("llllllllllllllllllllll", res);
+
       return res;
     } catch (e) {
       console.error("Error adding document: ", e);
@@ -88,6 +116,9 @@ export interface commentsProps {
     comment: string;
     commenter: string;
     timestamp: string;
+    likes: any[];
+    dislikes: any[];
+    commentLiked: boolean;
   };
 }
 
@@ -96,15 +127,19 @@ export const commentsInitialState = {
   comment: "",
   commenter: "",
   timestamp: "",
+  likes: [],
+  dislikes: [],
+  commentLiked: false,
 };
 
 export const commentsSlice = createSlice({
   name: "comments-redux",
   initialState: commentsInitialState,
   reducers: {
-    // commentRoute: (state, action) => {
-    //   state.commentRouteValue = action.payload;
-    // },
+    isCommentLiked: (state, action) => {
+      console.log("ghjgjghjghjgjghjgh", action.payload);
+      state.commentLiked = action.payload;
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(loadcomments.fulfilled, (state, action: any) => {
@@ -117,5 +152,5 @@ export const commentsSlice = createSlice({
 });
 
 export const getcommentsData = (state: commentsProps) => state.commentsStates;
-export const {} = commentsSlice.actions;
+export const { isCommentLiked } = commentsSlice.actions;
 export default commentsSlice.reducer;
